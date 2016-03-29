0b669b9264799eae340c9c31fdc412f49a6a7b31
Костя <eremenko.konstantin@gmail.com>
2016-03-29 13:39:23 +0300

    first

--- main/java/org/sourceit/Fraction.java
+++ main/java/org/sourceit/Fraction.java
@@ -2,6 +2,30 @@ package org.sourceit;
 
 public class Fraction {
 
-    public static void main(String[] args) {
+    private int num;
+    private int denom;
+
+    Fraction(){
+        this.num = 0;
+        this.denom = 0;
     }
+
+    public Fraction(int n, int d){
+        this.num = n;
+        this.denom = d;
+    }
+
+    public String toString(){
+        return this.num + "/" + this.denom;
+    }
+
+
+
+    public static void main(String[] args) {
+        Fraction a = new Fraction(1, 2);
+        Fraction b = new Fraction(3, 4);
+        System.out.println(b.toString());
+
+    }
+
 }
